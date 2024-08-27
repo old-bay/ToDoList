e757661907375ff88c81e0d12239b0350833f5d7
Old Bay <lancastermikael1994@gmail.com>
2024-08-26 21:13:30 -0400

    Commit for snowpack based app.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// import { v4 as uuidV4 } from 'uuid';
+// console.log(uuidV4());
+// console.log("hi");
 
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("#new-task-form") as HTMLFormElement;
+const title = document.querySelector<HTMLInputElement>("#new-task-title");
