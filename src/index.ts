803f3f30d13b5d96c8d85c832e33142aa5203f7e
Old Bay <lancastermikael1994@gmail.com>
2024-08-26 22:19:07 -0400

    final commit on the initial build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,59 @@
-// import { v4 as uuidV4 } from 'uuid';
-// console.log(uuidV4());
-// console.log("hi");
+import { v4 as uuidV4 } from 'uuid';
+console.log(uuidV4());
+console.log("hi");
+
+type Task =  {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById("#new-task-form") as HTMLFormElement;
-const title = document.querySelector<HTMLInputElement>("#new-task-title");
+const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+
+form?.addEventListener("submit", e => {
+  e.preventDefault();
+  if(input?.value == "" || input?.value == null) return
+  
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  tasks.push(newTask);
+
+  // TS can read implicit types
+  addListItem(newTask);
+  input.value = ""
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  })
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  const taskJSON = localStorage.getItem("TASKS");
+  if(taskJSON === null) return
+  return JSON.parse(taskJSON);
+}
